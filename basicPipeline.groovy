deff218d6742aa897d9943060c3b3734c5acbff2
José Reato Neto <josereato@outlook.com>
2018-11-12 16:28:10 -0200

    params

--- basicPipeline.groovy
+++ basicPipeline.groovy
@@ -1,10 +1,13 @@
 pipeline {
     agent any
-
+    parameters {
+        string(name: 'Greeting', defaultValue: 'Hello', description: 'How should I greet the world?')
+    }
     stages {
         stage('Build') {
             steps {
                 echo 'Building..'
+                echo "${params.Greeting} World!"
             }
         }
         stage('Test') {
