a36721c9a7c7ec8e9b57bd91c8fa43a82717cc6e
José Reato Neto <josereato@outlook.com>
2018-11-13 16:06:30 -0200

    payroll2

---
+++ Payroll2.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+    parameters {
+        string(name: 'branch', defaultValue: 'teste', description: 'Nome da branch')
+    }
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building...'
+                echo "Branch ${params.branch}"
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
